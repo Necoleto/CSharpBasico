755b91ddbdd096eccd8e7d951c9b85d590a5731c
Necoleto <46891882+Necoleto@users.noreply.github.com>
2019-03-03 13:38:26 +0100

    upgrade trabajando con arrays

---
+++ Project1/ConsoleApp1/Array01.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Array01
+    {
+        int[] lista;
+        int tam;
+        public Array01()
+        {
+        }
+        public void pedirTamano()
+        {
+            Console.WriteLine("Dime el numero de numeros que vas a introducir:");
+            this.tam = Convert.ToInt32(Console.ReadLine());
+            this.lista = new int[tam];
+
+        }
+        public void pedirNumeros()
+        {
+            Console.WriteLine("Introduce los numeros:");
+            for (int i = 0; i < this.lista.Length; i++)
+            {
+                lista[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void dameNumeroDiaAno()
+        {
+            Console.WriteLine("Selecciona un mes:");
+            int numMes = Convert.ToInt32(Console.ReadLine());
+            if (numMes > 12 || numMes < 0)
+            {
+                throw new Exception("ERROR: eleccion incorrecta");
+            }
+            Console.WriteLine("Selecciona un dia del mes:");
+            int numDia = Convert.ToInt32(Console.ReadLine());
+            if(numDia > this.lista[numMes-1] || numDia < 0)
+            {
+                throw new Exception("ERROR: eleccion incorrecta");
+            }
+            for (int i = 0; i < numMes-1; i++)
+            {
+                numDia += this.lista[i];
+            }
+            Console.WriteLine("Es el dia {0} del año.", numDia);
+            Console.WriteLine("Faltan {0} dias para final de año.",365-numDia);
+        }
+        public void ordenaNumeros()
+        {
+            Console.WriteLine("El nuevo orden es:");
+            this.lista = this.lista.Reverse().ToArray();
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine("{0}", this.lista[i]);
+            }
+        }
+
+
+
+    }
+}
--- Project1/ConsoleApp1/Program.cs
+++ Project1/ConsoleApp1/Program.cs
@@ -13,43 +13,58 @@ namespace ConsoleApp1
 
             #region -------------------MAIN OPERACIONES-----------------
 
-            int num1, num2;
+            //int num1, num2;
+            //try
+            //{
+            //    Console.WriteLine("Inserte el primer numero");
+            //    num1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Inserte el segundo numero");
+            //    num2 = Convert.ToInt32(Console.ReadLine());
+
+            //    IOperaciones operacion = new Suma(num1, num2);
+            //    int sumar = operacion.sumar();
+            //    Console.WriteLine("La suma de los numeros introducidos es: {0}", sumar);
+            //    Console.ReadLine();
+
+            //    operacion = new Resta(num1, num2);
+            //    int restar = operacion.restar();
+            //    Console.WriteLine("La resta de los numeros introducidos es: {0}", restar);
+            //    Console.ReadLine();
+
+            //    operacion = new Multiplicacion(num1, num2);
+            //    int multiplicar = operacion.multiplicar();
+            //    Console.WriteLine("La multiplicacion de los numeros introducidos es: {0}", multiplicar);
+            //    Console.ReadLine();
+
+            //    operacion = new Division(num1, num2);
+            //    decimal dividir = operacion.dividir();
+            //    Console.WriteLine("La division de los numeros introducidos es: {0}", dividir);
+            //    Console.ReadLine();
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("El valor introducido no es valido");
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("No se puede dividir un numero entre cero");
+            //}
+
+            #endregion
+
+            #region ---------------------MAIN ARRAYS------------------
+            Array01 prueba = new Array01();
             try
             {
-                Console.WriteLine("Inserte el primer numero");
-                num1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Inserte el segundo numero");
-                num2 = Convert.ToInt32(Console.ReadLine());
-
-                IOperaciones operacion = new Suma(num1, num2);
-                int sumar = operacion.sumar();
-                Console.WriteLine("La suma de los numeros introducidos es: {0}", sumar);
-                Console.ReadLine();
-
-                operacion = new Resta(num1, num2);
-                int restar = operacion.restar();
-                Console.WriteLine("La resta de los numeros introducidos es: {0}", restar);
-                Console.ReadLine();
-
-                operacion = new Multiplicacion(num1, num2);
-                int multiplicar = operacion.multiplicar();
-                Console.WriteLine("La multiplicacion de los numeros introducidos es: {0}", multiplicar);
-                Console.ReadLine();
-
-                operacion = new Division(num1, num2);
-                decimal dividir = operacion.dividir();
-                Console.WriteLine("La division de los numeros introducidos es: {0}", dividir);
-                Console.ReadLine();
+                prueba.pedirTamano();
+                prueba.pedirNumeros();
+                prueba.dameNumeroDiaAno();
+                //prueba.ordenaNumeros();
             }
             catch (FormatException)
             {
-                Console.WriteLine("El valor introducido no es valido");
+                Console.WriteLine("ERROR:Debe introducir numeros");
             }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("No se puede dividir un numero entre cero");
-            }
-
             #endregion
 
         }
